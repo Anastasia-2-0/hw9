78c080d63032ff6f0dd763bc800f9477a888599e
Anastasia-2-0 <nosova0702@mail.ru>
2022-11-13 11:58:16 +0300

    решение 2 задачи

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+ 
+
+Console.WriteLine("Введите число M:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+void GapNumberSum (int numberM, int numberN)
+{
+   
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN);
+}
+
+GapNumberSum(m, n);
