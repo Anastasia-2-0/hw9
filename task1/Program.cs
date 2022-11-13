01d9d4ca7baa5bd9ffebfc0f3d4011b1371dbdec
Anastasia-2-0 <nosova0702@mail.ru>
2022-11-13 11:39:56 +0300

    решение 1 задачи

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "4, 2"
+// N = 8 -> "8, 6, 4, 2,"
+
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+ChetNumbers(number);
+
+void ChetNumbers(int number)
+{
+    if (number < 0)
+    {
+        Console.Write($"{number} не натуральное число");
+    }
+    if (number % 2 == 0)
+    {
+        Console.Write(number);
+        Console.Write(",");
+    }
+    while (number > 1)
+    {
+    ChetNumbers(number - 1);
+    }
+}
